a4125f68f260e69d711617bc17209dd9d3d73c16
skshig <skshig@gmail.com>
2023-04-23 00:33:43 +0530

    sharable urls completed

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 // your code here
-let name_input = document.getElementById('name');
-
-let year_input = document.getElementById('year');
-
-let h3_content = document.getElementById('url');
-
 function sharableUrl(){
+    let name_input = document.getElementById('name');
+    let year_input = document.getElementById('year');
+    let h3_content = document.getElementById('url');
+    
     if(name_input && year_input){
-        h3_content.innerText += "?name=" + name_input.value + "?year=" + year_input.value;
+        h3_content.innerHTML =  "https://localhost:8080/";
+        h3_content.innerHTML += "?name=" + name_input.value + "&year=" + year_input.value;
+        
     }
     else if(name_input && !year_input){
-        h3_content.innerText += "?name=" + name_input.value;
+        h3_content.innerHTML += "?name=" + name_input.value; 
     }
     else{
-        h3_content.innerText = "?year=" + year_input.value
+        h3_content.innerHTML = "?year=" + year_input.value
     }
-    return h3_content.innerText;
+    return h3_content.innerHTML;
+   
 }
